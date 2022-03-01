6fa4b447236f5090b047523ecdbc18c8ce103d3d
João Gouveia <j.lucas.gouveia@gmail.com>
2022-03-01 12:37:51 +0000

    Add support for elapsed time

--- app/src/main/java/io/github/joaogouveia89/projecteuler/problems/base/Problem.kt
+++ app/src/main/java/io/github/joaogouveia89/projecteuler/problems/base/Problem.kt
@@ -7,12 +7,25 @@ abstract class Problem<T>(
     val hasBeenCalculated: Boolean
         get() = _hasBeenCalculated
 
+    val elapsedTime: Long
+        get() = _elapsedTime
+
+
     protected var _hasBeenCalculated = false
+
+    protected var _elapsedTime: Long = 0
 
     val result: T
         get() = _result
 
     protected abstract var _result: T
 
-    abstract fun solve()
+    protected abstract fun solutionImpl()
+
+    fun solve(){
+        val startTime = System.currentTimeMillis()
+        solutionImpl()
+        val endTime = System.currentTimeMillis()
+        _elapsedTime = endTime - startTime
+    }
 }
--- app/src/main/java/io/github/joaogouveia89/projecteuler/problems/solutions/MultiplesOf3AndFive.kt
+++ app/src/main/java/io/github/joaogouveia89/projecteuler/problems/solutions/MultiplesOf3AndFive.kt
@@ -9,7 +9,7 @@ object MultiplesOf3AndFive: Problem<Int>(
 
     override var _result: Int = 0
 
-    override fun solve()  {
+    override fun solutionImpl()  {
         var sum = 0
         for (i in 3..1000) {
             if (i % 3 == 0 || i % 5 == 0) sum += i
